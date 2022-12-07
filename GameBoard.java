c26ae67dc051ac19e8922017ed60d7ee953c4863
water <waterblah123@gmail.com>
2022-12-07 14:59:56 -0800

    added addCoin method

--- GameBoard.java
+++ GameBoard.java
@@ -2,4 +2,38 @@ public class GameBoard {
     private int columns = 7;
     private int colLength = 6;
     public Coin[][] board = new Coin[7][6];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // returns -1 if invalid, 1 if valid
+    public int addCoin(int x, int color){
+        for(int i = 0; i < 6; i++){
+            if (board[x][i].equals(null)){
+                board[x][i] = new Coin(color, x, i);
+                return 1;
+            }
+        }
+        return -1;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         GameBoard test = new GameBoard();
-        System.out.println(test.board);
+        test.board[1][2] = new Coin(1, 2, 1);
+        System.out.println(test.board[0][0]);
     }
 }
